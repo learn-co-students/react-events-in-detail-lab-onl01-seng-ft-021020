1be4be05154481ec8eb81a98899c45c3f0188e9a
Mikah Niehaus <Graywolf.niehaus@live.com>
2020-08-13 12:30:54 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,7 +1,8 @@
 // Code CoordinatesButton Component Here
 import React from 'react'
 
-
+//React lets you define components as classes or functions. Components defined as classes currently provide more features which
+// are described in detail on this page. To define a React component class, you need to extend React.Component:
 export default class DelayedButton extends React.Component{
     handleClick = event => {
         this.props.onReceiveCoordinates([event.clientX, event.clientY]);
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,9 +1,13 @@
 // Code DelayedButton Component Here
 import React from 'react';
-
+//React lets you define components as classes or functions. Components defined as classes currently provide more features which
+// are described in detail on this page. To define a React component class, you need to extend React.Component:
 class DelayedButton extends React.Component {
+  //its an on click event
   handleClick = event => {
+    //
     event.persist();
+    //asinc timeout
     window.setTimeout(() => {
       this.props.onDelayedClick(event);
     }, this.props.delay);
